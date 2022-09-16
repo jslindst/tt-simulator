88ef8938a62926f98e1f064279cfcb7963f9f6a5
jslindst <jukka.lindstrom@iki.fi>
2022-09-16 09:54:59 +0300

    changed the route to resourceTracker

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Simulator />} />
-          <Route path="resourceTracker" element={<ResourceTracker />} />
+          <Route path="/resourceTracker" element={<ResourceTracker />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
