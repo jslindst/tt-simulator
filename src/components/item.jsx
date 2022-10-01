a75990698d3a57aba2d9f1eb8ae03961048dd959
jslindst <jukka.lindstrom@iki.fi>
2022-10-02 00:20:05 +0300

    forgot item

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -1,23 +1,16 @@
 import React from "react";
 
-import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
-
+import { Draggable } from "@hello-pangea/dnd";
 import { Resource, Population, Capital } from "./TrackerIcons";
-
 import { Territory } from "../model/HistoryTracker.ts";
-
-
 import styled from "styled-components";
-
 import Chip from "@mui/material/Chip";
 import List from "@mui/material/List";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import ListItem from "@mui/material/ListItem";
-import { ListSubheader } from "@mui/material";
+import { BlockadeLevel } from "../model/HistoryTracker.ts";
 
 const ItemName = styled.div`
   width: 125px;
+  height: 29px;
   text-align: left;
   display: flex;
   align-items: center;
@@ -37,12 +30,13 @@ const Container = styled.div`
   margin-bottom: 4px;
   background-color: ${props => props.highlight ? 'white' : props.color}; 
   display: flex;
+  opacity: ${props => props.opacity};
   align-items: center;
   justify-content: flex-end;
 `;
 
 export default class TerritoryItem extends React.Component {
-  render() {
+  render() {    
     const territory = new Territory(this.props.territory);
     return (
       <Draggable
@@ -54,13 +48,14 @@ export default class TerritoryItem extends React.Component {
             {...provided.dragHandleProps}
             ref={provided.innerRef}
             isDragging={snapshot.isDragging}
-            color={territory.nation.color}
+            color={territory.blockadeLevel === BlockadeLevel.NONE ? territory.nation.color : "gray"}
             highlight={this.props.highlight}
+            onClick={this.props.onClick}
           >
             <ItemName bold={territory.isCapital()}>{territory.name}<Capital territory={territory} /></ItemName>
-            <ForcedDiv width="25px"><Population amount={territory.POP} /></ForcedDiv>
-            <ForcedDiv width="25px"><Resource amount={territory.RES} /></ForcedDiv>
-            <ForcedDiv width="25px"><Resource amount={territory.RESTransAfrica} color="red"/></ForcedDiv>
+            <ForcedDiv width="25px" style={{opacity: territory.blockadeLevel > 0 ? "20%" : "100%"}}><Population amount={territory.POP} /></ForcedDiv>
+            <ForcedDiv width="25px" style={{opacity: territory.blockadeLevel > 0 ? "20%" : "100%"}}><Resource amount={territory.RES} /></ForcedDiv>
+            <ForcedDiv width="25px" style={{opacity: territory.blockadeLevel > 1 ? "20%" : "100%"}}><Resource amount={territory.RESTransAfrica} color="red"/></ForcedDiv>
             <ForcedDiv width="60px">
             <Chip size="small" label={territory.nation.shortName} />
             </ForcedDiv>
